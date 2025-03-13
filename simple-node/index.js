d349a108c7b4affb4c178b231574694705bc37c8
V-R-DeLosSantos <valentino.r.delossantos@outlook.com>
2025-03-13 19:31:59 -0300

    Recibiendo información

--- simple-node/index.js
+++ simple-node/index.js
@@ -19,6 +19,8 @@ let notes = [
   }
 ]
 
+app.use(express.json())
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -27,16 +29,49 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
-  
   if (note) {
     response.json(note)
   } else {
+    console.log('x')
     response.status(404).end()
   }
-  
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
